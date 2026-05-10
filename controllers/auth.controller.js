122fa5e45a5a39955850d8fbdc5c6757b89c4f81
thecyberdev <thecyberdevvarun@gmail.com>
2026-05-10 19:13:14 +0530

    implement the logout and lagout from all devices feature by session management.

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { config } from "../config/config.js";
 import userModel from "../models/user.model.js";
-import jwt from "jsonwebtoken";
+import sessionModel from "../models/session.model.js";
+import jwt, { decode } from "jsonwebtoken";
 
 export async function register(req, res) {
   try {
@@ -30,13 +31,29 @@ export async function register(req, res) {
     });
 
     // 3. Create a Token.
-    const accessToken = jwt.sign({ id: user._id }, config.JWT_SECRET, {
-      expiresIn: "15m",
-    });
-
     const refreshToken = jwt.sign({ id: user._id }, config.JWT_SECRET, {
       expiresIn: "7d",
     });
+
+    const refreshTokenHash = crypto
+      .createHash("sha256")
+      .update(refreshToken)
+      .digest("hex");
+
+    const session = await sessionModel.create({
+      userId: user._id,
+      refreshTokenHash,
+      ip: req.ip,
+      userAgent: req.headers["user-agent"],
+    });
+
+    const accessToken = jwt.sign(
+      { id: user._id, sessionId: session._id },
+      config.JWT_SECRET,
+      {
+        expiresIn: "15m",
+      },
+    );
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
@@ -52,6 +69,70 @@ export async function register(req, res) {
     });
   } catch (error) {
     console.log("Something went wrong while creating User.", error);
+  }
+}
+
+export async function login(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({
+      email,
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "Invalid email or password." });
+    }
+
+    const hashedPassword = crypto
+      .createHash("sha256")
+      .update(password)
+      .digest("hex");
+
+    const isPasswordValid = hashedPassword === user.password;
+
+    if (!isPasswordValid) {
+      return res.status(404).json({ message: "Invalid email or password." });
+    }
+
+    const refreshToken = jwt.sign({ id: user._id }, config.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    const refreshTokenHash = crypto
+      .createHash("sha256")
+      .update(refreshToken)
+      .digest("hex");
+
+    const session = await sessionModel.create({
+      userId: user._id,
+      refreshTokenHash,
+      ip: req.ip,
+      userAgent: req.headers["user-agent"],
+    });
+
+    const accessToken = jwt.sign(
+      { id: user._id, sessionId: session._id },
+      config.JWT_SECRET,
+      {
+        expiresIn: "15m",
+      },
+    );
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    });
+
+    res.status(201).json({
+      message: "User login successfully.",
+      user: { username: user.username, email: user.email },
+      accessToken,
+    });
+  } catch (error) {
+    console.log("Something went wrong while login User.", error);
   }
 }
 
@@ -85,6 +166,22 @@ export async function refreshToken(req, res) {
 
     const decoded = jwt.verify(refreshToken, config.JWT_SECRET);
 
+    const refreshTokenHash = crypto
+      .createHash("sha256")
+      .update(refreshToken)
+      .digest("hex");
+
+    const session = await sessionModel.findOne({
+      refreshTokenHash,
+      revoked: false,
+    });
+
+    if (!session) {
+      return res.status(401).json({
+        message: "Invalid refresh token",
+      });
+    }
+
     const accessToken = jwt.sign(
       {
         id: decoded.id,
@@ -97,6 +194,14 @@ export async function refreshToken(req, res) {
       expiresIn: "7d",
     });
 
+    const newRefreshTokenHash = crypto
+      .createHash("sha256")
+      .update(newRefreshToken)
+      .digest("hex");
+
+    session.refreshTokenHash = newRefreshTokenHash;
+    await session.save();
+
     res.cookie("refreshToken", newRefreshToken, {
       httpOnly: true,
       secure: true,
@@ -107,5 +212,68 @@ export async function refreshToken(req, res) {
     res
       .status(200)
       .json({ message: "Access Token refresh successfully.", accessToken });
+  } catch (error) {}
+}
+
+export async function logout(req, res) {
+  try {
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) {
+      return res.status(400).json({
+        message: "Refresh token not found",
+      });
+    }
+
+    const refreshTokenHash = crypto
+      .createHash("sha256")
+      .update(refreshToken)
+      .digest("hex");
+
+    const session = await sessionModel.findOne({
+      refreshTokenHash,
+      revoked: false,
+    });
+
+    if (!session) {
+      return res.status(400).json({
+        message: "Invalid refresh token.",
+      });
+    }
+
+    session.revoked = true;
+    await session.save();
+
+    res.clearCookie("refreshToken");
+
+    res.status(200).json({
+      message: "Logged out successfully.",
+    });
+  } catch (error) {}
+}
+
+export async function logoutAll(req, res) {
+  try {
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) {
+      return res.status(400).json({
+        message: "Refresh token not found",
+      });
+    }
+
+    const decoded = jwt.verify(refreshToken.config.JWT_SECRET);
+
+    await sessionModel.updateMany(
+      {
+        user: decoded.id,
+        revoked: false,
+      },
+      { revoked: true },
+    );
+
+    res.clearCookie("refreshToken");
+
+    res.status(200).json({
+      message: "Logged out from all devices successfully.",
+    });
   } catch (error) {}
 }
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -4,7 +4,10 @@ import * as authController from "../controllers/auth.controller.js";
 const authRouter = Router();
 
 authRouter.post("/register", authController.register);
-authRouter.get("/get-me", authController.getMe);
+authRouter.post("/login", authController.login);
 authRouter.get("/refresh-token", authController.refreshToken);
+authRouter.get("/get-me", authController.getMe);
+authRouter.get("/logout", authController.logout);
+authRouter.get("/logout-all", authController.logoutAll);
 
 export default authRouter;
